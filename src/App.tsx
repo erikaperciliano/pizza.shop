d123e53abfe71fdee9dbf796bba141e0cb8a52fc
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-03-24 19:01:06 +0000

    adds shadcn/ui and creates routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { RouterProvider } from "react-router-dom"
+import './global.css'
+import { router } from "./routes"
 
 export function App() {
 
   return (
-    <>
-      <h1>Pizz Shop</h1>
-    </>
+      <RouterProvider router={router}/>
   )
 }
 
