7ac69cf4098253b856497a573c55e45ded3bc2c9
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-04-02 20:00:24 +0100

    fix: adjusts the routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,16 +2,17 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 
 export const router = createBrowserRouter([
     {
         path: '/', 
         element: <AppLayout/>,
-        children: [{ path: '/', element: <Dashboard/> }]
+        children: [{ path: '', element: <Dashboard/> }]
     },
     {
-        path: '/', 
-        element: <AppLayout/>,
-        children: [{ path: '/sing-in', element: <SignIn/> }]
+        path: '/sign-in', 
+        element: <AuthLayout />,
+        children: [{ path: '', element: <SignIn/> }]
     },
 ])
