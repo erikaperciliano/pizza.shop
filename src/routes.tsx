42ce09e5e13187471ada12538a1cb0b10ddb61a0
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-04-03 21:31:10 +0100

    feat: adds Register Page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
     {
@@ -14,5 +15,10 @@ export const router = createBrowserRouter([
         path: '/sign-in', 
         element: <AuthLayout />,
         children: [{ path: '', element: <SignIn/> }]
+    },
+    {
+        path: '/sign-up', 
+        element: <AuthLayout />,
+        children: [{ path: '', element: <SignUp/> }]
     },
 ])
