b09fe50ca027469dd79574ea724fba46dcfe210b
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-04-04 16:45:34 +0100

    feat: add Theme to the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './global.css'
 import { router } from "./routes"
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from "./components/theme/theme.provider"
 
 export function App() {
 
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop"/>
-      <Toaster richColors/>
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizza-shop-theme" defaultTheme="system">
+        <Helmet titleTemplate="%s | pizza.shop"/>
+        <Toaster richColors/>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
