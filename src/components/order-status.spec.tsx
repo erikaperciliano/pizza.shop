bff3a2838a8b4f12ccc1219c71d456bb7a6e4002
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-04-22 17:15:02 +0100

    test: add unit tests to order status

--- src/components/order-status.spec.tsx
+++ src/components/order-status.spec.tsx
@@ -1,6 +1,85 @@
-import { test, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { OrderStatus } from './order-status'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-test('1 + 1 equals 2 ', () => {
-    expect(1 + 1).toEqual(2)
+describe('Oder Status', () => {
+    it('should display the right text when order status is pending', () => {
+        /* Pending */
+        const wrapper = render(<OrderStatus status='pending'/>)
+
+        // wrapper.debug()
+
+        const statusText = wrapper.getByText('Pendente')
+        const badgeElement = wrapper.getByTestId('badge')
+        // console.log(badgeElement.outerHTML)
+
+        
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-slate-400')
+
+        
+    })
+
+    it('should display the right text when order status is canceled', () => {
+        /* Canceled */
+        const wrapper = render(<OrderStatus status='canceled'/>)
+
+        // wrapper.debug()
+
+        const statusText = wrapper.getByText('Cancelado')
+        const badgeElement = wrapper.getByTestId('badge')
+        // console.log(badgeElement.outerHTML)
+
+        
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-rose-500')
+    })
+    
+    it('should display the right text when order status is delivering', () => {
+        /* Canceled */
+        const wrapper = render(<OrderStatus status='delivering'/>)
+
+        // wrapper.debug()
+
+        const statusText = wrapper.getByText('Em entrega')
+        const badgeElement = wrapper.getByTestId('badge')
+        // console.log(badgeElement.outerHTML)
+
+        
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-amber-500')
+    })
+      
+    it('should display the right text when order status is processing', () => {
+        /* Canceled */
+        const wrapper = render(<OrderStatus status='processing'/>)
+
+        // wrapper.debug()
+
+        const statusText = wrapper.getByText('Em preparo')
+        const badgeElement = wrapper.getByTestId('badge')
+        // console.log(badgeElement.outerHTML)
+
+        
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-amber-500')
+    })
+
+    it('should display the right text when order status is delivered', () => {
+        /* Canceled */
+        const wrapper = render(<OrderStatus status='delivered'/>)
+
+        // wrapper.debug()
+
+        const statusText = wrapper.getByText('Entregue')
+        const badgeElement = wrapper.getByTestId('badge')
+        // console.log(badgeElement.outerHTML)
+
+        
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-emerald-500')
+    })
+
 })
 
