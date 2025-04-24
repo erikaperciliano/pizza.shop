f9c6930c54dc2f4409a416051e5fe2ec66c64b85
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-04-24 22:35:12 +0100

    test: adds mocks of the orders

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -11,8 +11,24 @@ import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
 import { getProfileMock } from './get-profile-mock';
 import { getManagedRestaurantMock } from './get-managed-restaurant-mock';
 import { updateProfiletMock } from './update-profile-mock';
+import { getOrdersMock } from './get-orders-mock';
+import { getOrderDetailsMock } from './get-order-details-mock';
 
-export const worker = setupWorker(signInMock, registerRestaurantMock, getDayOrdersAmountMock, getMonthOrdersAmountMock, getMonthRevenueMock, getMonthCanceledOrdersAmountMock, getPopularProductsMock, getDailyRevenueInPeriodMock, getProfileMock, getManagedRestaurantMock, updateProfiletMock)
+export const worker = setupWorker(
+    signInMock, 
+    registerRestaurantMock, 
+    getDayOrdersAmountMock, 
+    getMonthOrdersAmountMock, 
+    getMonthRevenueMock, 
+    getMonthCanceledOrdersAmountMock, 
+    getPopularProductsMock, 
+    getDailyRevenueInPeriodMock, 
+    getProfileMock, 
+    getManagedRestaurantMock, 
+    updateProfiletMock,
+    getOrdersMock,
+    getOrderDetailsMock
+)
 
 export async function enableMSW() {
     if(env.MODE !== 'test') {
