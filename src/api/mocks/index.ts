48fd1837bfec213c8565eb6f6ff2ba7082eec287
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-04-24 22:45:05 +0100

    test: adds mock of the actions

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -13,6 +13,10 @@ import { getManagedRestaurantMock } from './get-managed-restaurant-mock';
 import { updateProfiletMock } from './update-profile-mock';
 import { getOrdersMock } from './get-orders-mock';
 import { getOrderDetailsMock } from './get-order-details-mock';
+import { approveOrderMock } from './approve-order-mock';
+import { cancelOrderMock } from './cancel-order-mock';
+import { deliverOrderMock } from './deliver-order-mock';
+import { dispatchOrderMock } from './dispatch-order-mock';
 
 export const worker = setupWorker(
     signInMock, 
@@ -27,7 +31,11 @@ export const worker = setupWorker(
     getManagedRestaurantMock, 
     updateProfiletMock,
     getOrdersMock,
-    getOrderDetailsMock
+    getOrderDetailsMock,
+    approveOrderMock,
+    cancelOrderMock,
+    deliverOrderMock,
+    dispatchOrderMock,
 )
 
 export async function enableMSW() {
